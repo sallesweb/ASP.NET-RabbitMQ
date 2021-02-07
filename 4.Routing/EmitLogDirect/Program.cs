2a3b404a2490bc489e248d8abb70cbfd4b217742
Felipe Emygdio de Salles <felipesalles.ti@gmail.com>
2021-02-07 00:36:36 -0300

    Criação de projeto para receber mensagens seletivamente através do exchange type: Direct

---
+++ 4.Routing/EmitLogDirect/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace EmitLogDirect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "direct_logs", type: ExchangeType.Direct);
+
+                var severity = (args.Length > 0) ? args[0] : "info";
+                var message = GetMessage(args);
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(exchange: "direct_logs", routingKey: severity, basicProperties: null, body: body);
+
+                Console.WriteLine(" [x] Sent '{0}':'{1}'", severity, message);
+            }
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return ((args.Length > 1)
+                ? string.Join(" ", args.Skip(1).ToArray())
+                : "Hello World!");
+        }
+    }
+}
---
+++ 4.Routing/README.md
@@ -0,0 +1,18 @@
+# ASP.NET-Routing
+O objetivo deste projeto é criar aplicativos de mensagens para assinar apenas um subconjunto específico de mensagens, por exemplo direcionar logs críticos para arquivo de log visando economia de espaço em disco e exibir todos os tipos de log no console.  
+Foram criados dois projetos baseados no template Application Console do framework .NET 5:
+
+- *Producer (NewTask):* aplicação que envia mensagens;
+- *Exchange:* intermedia a passagem de mensagens do *Producer* para a lista;
+- *Queue (RabbitMQ):* buffer que armazena mensagens;
+- *Consumer (Worker):* aplicação que recebe mensagens.
+
+## Exchange
+O *Producer* enviará as mensagens para uma *exchange*, seu funcionamento é simples, por um lado recebe as mensagens do *Producer* e, por outro, envia as mensagens para a fila.
+
+### Tipos de exchange: **Direct**
+Transmite a mensagem para as listas cuja *binding key* seja exatamente igual à *routing key* da mensagem.
+
+## Referências
+- [Downloading and Installing RabbitMQ](https://www.rabbitmq.com/download.html)
+- [Publish/Subscribe](https://www.rabbitmq.com/tutorials/tutorial-three-dotnet.html)
